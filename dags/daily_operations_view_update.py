af8c3e0ee066d57e1cde381965c170e49402b254
Jake Roach <jroachgolf84@macbook-pro.mynetworksettings.com>
2023-11-30 21:39:09 -0500

    Stashing initial changes to second DAG

---
+++ dags/daily_operations_view_update.py
@@ -0,0 +1,16 @@
+"""
+daily_operations_view_update.py
+
+Description:
+    File that is used to instantiate the daily_operations_view_update DAG. This DAG runs four SQL store-procedures
+    concurrently, similar to a task a Data Engineer, Analyst, or Scientist may have to run daily at the beginning of
+    each work day if there weren't to have a tool like Airflow.
+
+Author: Jake Roach
+Date: 2023-11-30
+"""
+
+# Import modules here
+
+
+# Last line of file
